4e47a9f06ee158247e73c1a27252da233be654a0
lucas271102 <lucas001spe@gmail.com>
2024-04-22 22:12:20 -0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-    <div className='bg-black w-full'>
-        <h1 className='text-white text-center p-6 text-4xl font-extralight font-sans'>Horizon Solutions</h1>
+    <div className='bg-slate-900 w-full'>
+        <h1 className='text-white text-center p-6 text-4xl font-extralight  font-sans'>Horizon Solutions</h1>
     </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans':['Geologica','sans-serif' ]
+      }
+    },
   },
   plugins: [],
 }
