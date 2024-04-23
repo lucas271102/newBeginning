96771aa958eaa4e16bacd9db3b84290da3482761
lucas271102 <lucas001spe@gmail.com>
2024-04-23 12:18:02 -0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import Footer from './components/Footer'
+import Layout from './layout/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+  <Home/>
+  
      
-     <NavBar/>
-     <Home/>
+
+     
+    
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+
+
+    <div className='bg-slate-900 '>
+
+    <h1 className='text-center text-white font-sans'>Footer</h1>
+    </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
