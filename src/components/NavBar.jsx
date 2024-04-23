f43cb16ed7e3e09cac1fc13e5b018408bc6ff5da
lucas271102 <lucas001spe@gmail.com>
2024-04-22 21:44:50 -0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-    
+    <div className='bg-black w-full'>
+        <h1 className='text-white text-center p-6 text-4xl font-extralight font-sans'>Horizon Solutions</h1>
+    </div>
     </>
   )
 }
