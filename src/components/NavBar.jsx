f0c8e6e0e47b8f133b70852aeed565edf915da85
lucas271102 <lucas001spe@gmail.com>
2024-04-25 21:39:54 -0300

    landing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-    <div className='bg-slate-950  w-full shadow-inner'>
-        <h1 className='text-white text-center p-6 text-4xl font-extralight  font-sans'>Horizon Solutions</h1>
+    <div className='w-full shadow-2xl'>
+        <h1 className='text-white text-center p-6 text-2xl md:text-6xl  font-extralight  font-sans'>Horizon Solutions</h1>
     </div>
     </>
   )
