e047ee9e6a2e42c2e516b7e85f7bb3805aef2f52
lucas271102 <lucas001spe@gmail.com>
2024-05-11 22:15:37 -0300

    landing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,12 +21,43 @@ const Home = () => {
 
     <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
       <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-lime-500/10 hover:shadow-lime-500/10"
+        href="#"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="size-10 text-lime-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+          distinctio alias voluptatum blanditiis laudantium.
+        </p>
+      </a>
+
+      <a
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
         href="#"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
+          className="size-10 text-lime-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -88,7 +119,7 @@ const Home = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
+          className="size-10 text-lime-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -119,7 +150,7 @@ const Home = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
+          className="size-10 text-lime-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -150,38 +181,7 @@ const Home = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
-
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
-
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
+          className="size-10 text-lime-500"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -210,7 +210,7 @@ const Home = () => {
     <div className="mt-12 text-center ">
       <a
         href="https://calendly.com/lucasspeziale/dynamedia"
-        className="inline-block rounded-3xl bg-pink-600 px-12 py-3 sm:text-3xl font-normal font-sans text-white transition hover:bg-pink-700 focus:outline-none focus:ring shadow-xl focus:ring-yellow-400 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        className="inline-block rounded-3xl bg-lime-600 px-12 py-3 sm:text-3xl font-normal font-sans text-white transition hover:bg-lime-500 focus:outline-none focus:ring shadow-xl focus:ring-yellow-400 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
       >
         <div className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
 
